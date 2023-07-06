3a62ed9b0a9e5cdc240dc4007c7727f2352613c1
alphasigma25 <61196412+alphasigma25@users.noreply.github.com>
2023-07-06 23:27:23 +0200

    ajout exceptions

--- GraphManager/Model/Node.cs
+++ GraphManager/Model/Node.cs
@@ -8,6 +8,12 @@ public class Node {
                 e.N1.Edges.Remove(e);
             } else if (e.N2 != this) {
                 e.N2.Edges.Remove(e);
+            } else {
+                // on préfère qu'il y ait une erreur ici car
+                // en écrivant le code je suis parti du principe qu'on arriverait
+                // jamais ici, mais si un jour le code change, ça serait bien 
+                // prévenir
+                throw new Exception("Edge not linked to node edge list");
             }
         });
         Edges.Clear();
@@ -21,6 +27,8 @@ public class Node {
                 yield return e.N2;
             } else if (e.N2 == this) {
                 yield return e.N1;
+            } else {
+                throw new Exception("Edge not linked to node in edge list");
             }
         };
     }
