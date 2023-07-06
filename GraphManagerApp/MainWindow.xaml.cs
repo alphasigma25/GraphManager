0c814f727ff59b223c5395797c521e11d4f972c1
alphasigma25 <61196412+alphasigma25@users.noreply.github.com>
2023-07-06 11:59:39 +0200

    start

---
+++ GraphManagerApp/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace GraphManagerApp;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window {
+    public MainWindow() {
+        InitializeComponent();
+    }
+
+    private void SelectOrAction(object sender, MouseButtonEventArgs e) {
+        if (e.OriginalSource is Shape) {
+            Shape activeRec = (Shape)e.OriginalSource;
+            MyCanvas.Children.Remove(activeRec);
+        } else {
+            AddCircle(MyCanvas,
+                Mouse.GetPosition(MyCanvas).X,
+                Mouse.GetPosition(MyCanvas).Y, 50);
+        }
+    }
+
+    private void AddRectangle(Canvas myCanvas, double cx, double cy, int w, int h) {
+        Rectangle newRec = new Rectangle {
+            Width = w,
+            Height = h,
+            Fill = Brushes.White,
+            StrokeThickness = 1,
+            Stroke = Brushes.Black
+        };
+
+        Canvas.SetLeft(newRec, cx);
+        Canvas.SetTop(newRec, cy);
+        myCanvas.Children.Add(newRec);
+    }
+    private void AddCircle(Canvas myCanvas, double cx, double cy, double r) {
+        Ellipse newCir = new Ellipse {
+            Width = r,
+            Height = r,
+            Fill = Brushes.White,
+            StrokeThickness = 1,
+            Stroke = Brushes.Black
+        };
+
+        Canvas.SetLeft(newCir, cx - r / 2);
+        Canvas.SetTop(newCir, cy - r / 2);
+        myCanvas.Children.Add(newCir);
+    }
+}
+
+
